44ea19267c330901340b10e849fe39d874f2b6ac
angelbelando <angel.belando@orange.fr>
2019-03-19 15:32:06 +0100

    Déploiement Sweet Beauty

--- soins/admin.py
+++ soins/admin.py
@@ -5,5 +5,6 @@ admin.site.register(Family_Care)
 
 class CareAdmin(admin.ModelAdmin):
     search_fields = ['name',]
+    fields = ["name", "name_it", "family", 'price', 'display_order']
 
 admin.site.register(Care, CareAdmin)
